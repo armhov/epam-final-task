3f682d76376a069ea7b31ccdc4297856503d7d52
Armen Hovhannisyan <33937449+armhov@users.noreply.github.com>
2021-07-06 16:42:39 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-east-2"
-  access_key  = "AKIAURX525MLMMJVVUFU"
-  secret_key  = "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"
 }
 
 data "aws_ami" "latest-ubuntu-ami" {
