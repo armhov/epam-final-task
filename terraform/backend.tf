1332c060b6435c9834ab308677bbd2097b241ab1
Armen Hovhannisyan <33937449+armhov@users.noreply.github.com>
2021-07-06 17:25:17 +0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "epam-task-terraform-backend"
     key    = "task/terraform.tfstate"
     region = "us-east-2"
+    access_key = 'AKIAURX525MLMMJVVUFU'
+    secret_key ='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'
   }
 }
